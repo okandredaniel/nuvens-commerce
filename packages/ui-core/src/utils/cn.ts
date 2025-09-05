b72f61440764265adc01786fbc6eac6dda8713b2
okandredaniel <129803135+okandredaniel@users.noreply.github.com>
2025-09-05 09:55:12 +0100

    improve cn to a better handling of class names

--- packages/ui-core/src/utils/cn.ts
+++ packages/ui-core/src/utils/cn.ts
@@ -1,3 +1,7 @@
-export function cn(...parts: Array<string | undefined | null | false>) {
-  return parts.filter(Boolean).join(' ');
+import { type ClassValue } from 'clsx';
+import clsx from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
 }
