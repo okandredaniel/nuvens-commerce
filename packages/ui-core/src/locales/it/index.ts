e910463356539ccbcd4c46c4f7c0936affe6b579
okandredaniel <129803135+okandredaniel@users.noreply.github.com>
2025-09-05 18:00:26 +0100

    add new translations

--- packages/ui-core/src/locales/it/index.ts
+++ packages/ui-core/src/locales/it/index.ts
@@ -1,6 +1,7 @@
 import cart from './cart.json';
 import catalog from './catalog.json';
 import common from './common.json';
+import contact from './contact.json';
 import product from './product.json';
 
-export default { cart, catalog, common, product };
+export default { cart, catalog, common, contact, product };
